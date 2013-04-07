e844c76f80c6a02cadbf5b86a5ba9887ec6364c5
RinaYaya <yacine.oleemahomed@club-internet.fr>
2013-04-07 23:22:44 +0200

    ouvrir et fermer un fichier

--- Projet.s
+++ Projet.s
@@ -9,3 +9,17 @@ la $a0, fout
 li $a1, 0
 li $a2,0
 syscall
+
+move $s0, $v0
+
+# reading from file just opened
+    li   $v0, 14       # system call for reading from file
+    move $a0, $s0      # file descriptor 
+    la   $a1, buffer   # address of buffer from which to read
+    li   $a2, 100000   # hardcoded buffer length
+    syscall            # read from file
+
+    # Close the file 
+    li   $v0, 16       # system call for close file
+    move $a0, $s0      # file descriptor to close
+    syscall            # close file
